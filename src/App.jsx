cbf2922ae2e40873aa9d0d3a2af48814cd017b1e
Georgios <georgios.p.kondylis@gmail.com>
2025-07-06 16:55:27 +0200

    working on the hero i ve done the letters and leafs animations and now its time for the video

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/Navbar";
+import { Navbar, Hero } from "./components/exports";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-   <main>
-    <Navbar />
-   </main>
+    <>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-amber-900"></div>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,31 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-
 import { navLinks } from "../../constants/index.js";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
   return (
     <nav>
-      <div className="transition1">
+      <div>
         <a href="#home" className="flex items-center gap-2">
           <img src="/images/logo.png" alt="logo" />
           <p>Velvet Pour</p>
